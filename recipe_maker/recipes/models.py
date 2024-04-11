3106f83d6fb14e1c55cc887f6992311649d2fcd2
joDis06 <jdisomma40@stuy.edu>
2024-04-11 14:50:14 -0400

    Initialized UserAuth, Models, LoginPage, stuff.

--- recipe_maker/recipes/models.py
+++ recipe_maker/recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+    		return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    ingredients = models.ManyToManyField(Ingredient)
+    foodDescription = models.CharField(max_length=500)
+    recipe = models.CharField(max_length=500)
+    
+    class Meta:
+        ordering = ['name']
+        
+    
+        
+    
--- recipe_maker/recipes/views.py
+++ recipe_maker/recipes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from django.forms.models import model_to_dict
+
+from .models import Ingredient, Recipe
+from .forms import RecipeForm
+
+# Views
+
+## Recipes
+
+class RecipeListView(LoginRequiredMixin, ListView):
+    model = Recipe
