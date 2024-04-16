e835356ccc14793cd4abfabdf4448eb7b442f9a4
Adeeb <adeeb19909@gmail.com>
2024-04-16 14:21:38 -0400

    created basic form fields through VUE, CSRF token errors still exist and are being addressed

---
+++ recipe_maker_vue/src/apps/recipe_edit/RecipeEdit.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Create Recipes Below!</h1>
+    </div>
+
+    <div>
+        <form method="post" class="form">
+            <input type="hidden" name="csrfmiddlewaretoken"
+                   v-bind:value="csrf_token">
+            <p>
+                <label for="id_name">Recipe Name:</label>
+                <input type="text" maxlength="100" required="" id="id_name">
+            </p>
+
+            <p>
+                <label for="id_director">Food Description:</label>
+                <input type="text" maxlength="500" required="" id="id_food_description">
+            </p>
+
+                <button type="submit" class="btn btn-primary">
+                    Submit
+                </button>                    
+        </form>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'App',
+  components: {
+  },
+  props: [
+  ],
+  data: function() {
+    return {
+        csrf_token: ext_csrf_token,
+    }
+  },
+  computed: {
+  },
+  methods: {
+  },
+  watch: {
+  }
+}
+</script>
+
+
--- recipe_maker_vue/vite.config.js
+++ recipe_maker_vue/vite.config.js
@@ -1,16 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const backendPath = '../movie_theater';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue()
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  base: '/static/vite/',
+  server: {
+    watch: {
+      ignored: [],
+    },
+  },
+  build: {
+    manifest: true,
+    emptyOutDir: true,
+    outDir: `${backendPath}/core/static/vite/`,
+    rollupOptions: {
+      input: {
+        vue_recipe_edit: "./src/apps/recipe_edit/recipe_edit.js",
+      },
+    },
+  },
+});
