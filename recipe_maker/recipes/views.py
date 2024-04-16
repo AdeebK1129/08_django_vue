6fb9adfe7d49bc4e7dd7b03e71a822b62d303be2
joDis06 <jdisomma40@stuy.edu>
2024-04-16 14:55:45 -0400

    update; Fix up recipeedit; do recipeshow

--- recipe_maker/recipes/views.py
+++ recipe_maker/recipes/views.py
@@ -1,5 +1,3 @@
-from os import path
-from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
@@ -18,8 +16,66 @@ from .forms import RecipeForm
 
 # Views
 
-## Recipes
+## Ingredients
 
+class IngredientListView(LoginRequiredMixin, ListView):
+    model = Recipe
+
+class IngredientCreateView(CreateView):
+    model = Ingredient
+    fields = ['name']
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            'Ingredient "{ingredient_name}" has been created'.format(
+                ingredient_name=self.object.name))
+        return response
+
+    def get_success_url(self):
+    	return reverse_lazy("recipes:recipe_detail", args=[self.object.id])
+    
+class IngredientDetailView(DetailView):
+    model = Ingredient
+
+class IngredientUpdateView(UpdateView):
+    model = Ingredient
+    fields = ['name']
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            'Ingredient "{ingredient_name}" has been updated'.format(
+                ingredient_name=self.object.name))
+        return response
+    
+    # comment the following line to show the error about not having an
+    # success_url
+    def get_success_url(self):
+        return reverse_lazy("recipes:ingredient_detail", args=[self.object.id])
+        # you can also use it this way with kwargs, just to let you know
+        # but here we have only one argument, so no mistake can be done
+        #return reverse_lazy("movies:actor_detail",
+        #                    kwargs={'pk':self.object.id})
+    
+
+class IngredientDeleteView(DeleteView):
+    model = Ingredient
+    success_url = reverse_lazy("recipes:ingredient_list")
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            'Ingredient "{ingredient_name}" has been deleted'.format(
+                actor_name=self.object.name))
+        return response
+    
+
+# Recipes 
+    
 class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
 
@@ -37,29 +93,57 @@ class RecipeCreateView(CreateView):
             'Recipe "{recipe_name}" has been created'.format(
                 recipe_name=self.object.name))
         return response
+    
+    
+# comment the following line to show the error about not having an
+# success_url
+    def get_success_url(self):
+        return reverse_lazy("recipes:recipe_detail", args=[self.object.id])
+    # you can also use it this way with kwargs, just to let you know
+    # but here we have only one argument, so no mistake can be done
+    #return reverse_lazy("movies:actor_detail",
+    #                    kwargs={'pk':self.object.id})
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    form_class = RecipeForm
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            f'Recipe "{self.object.name}" has been updated'
+        )
+        return response
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        recipe_dico = model_to_dict(self.object)
+        ingredients = recipe_dico["recipes"]
+        recipe_ingredient_list = [{"id": recipe.id, "name": recipe.name} for recipe in recipes]
+        recipe_dico["ingredients"] = recipe_ingredient_list
+        ingredient_list = list(Ingredient.objects.all().values())
+        context["recipe_dict"] = recipe_dico
+        context["ingredient_list"] = ingredient_list
+        print("context", context)
+        return context
 
     def get_success_url(self):
         return reverse_lazy("recipes:recipe_detail", args=[self.object.id])
+        # You can also use it this way with kwargs, just to let you know
+        # but here we have only one argument, so no mistake can be done
+        # return reverse_lazy("movies:actor_detail", kwargs={'pk': self.object.id})
 
-class RecipeUpdatebisView(View):
-   def post(self, request, *args, **kwargs):
-       movie = get_object_or_404(Recipe, pk=self.kwargs["pk"])
-       # Create a form instance with POST data
-       form = RecipeForm(request.POST, instance=movie)
-       if form.is_valid():
-           form.save()
-           return JsonResponse({"success": True})
-       else:
-           return JsonResponse({"success": False, "errors": form.errors})
 
-class RecipeDetailbisView(TemplateView):
-    template_name = "recipes/recipe_detailbis.html"
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    success_url = reverse_lazy("recipes:recipe_list")
     
-    def get(self, request, *args, **kwargs):
-        movie = get_object_or_404(Recipe, pk=self.kwargs["pk"])
-        return super().get(request, *args, **kwargs)
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['movie_id'] = self.kwargs["pk"]
-        return context
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            'Recipe "{recipe_name}" has been deleted'.format(
+                recipe_name=self.object.name))
+        return response
